fcae3a864408cb02d0548680f650669f584f2016
hanchaoqi <hanchao_qi@qq.com>
2016-04-30 05:42:50 -0400

    fix some bugs about findall() and LinkExtractor()

--- commitsSpider/spiders/githubSpider.py
+++ commitsSpider/spiders/githubSpider.py
@@ -13,17 +13,17 @@ class GithubspiderSpider(CrawlSpider):
     def __init__(self,software):
         self.software = software
         if software == "linux":
-            self.start_urls = ['https://github.com/torvalds/linux/commits/master?page=1']
-            self.rules = (
-        Rule(LinkExtractor(allow=r'https://github.com/torvalds/linux/commits/master\?page=\d+',restrict_xpaths=('//div[@class="pagination"]/a[last()]'))),
-        Rule(LinkExtractor(allow=r'/torvalds/linux/commit/[a-f0-9]+',restrict_xpaths=('//p[@class="commit-title"]/a[@class="message"]')), callback='parse_item', follow=True),
-        )
+            self.start_urls = ['https://github.com/torvalds/%s/commits/master' % software,'https://github.com/torvalds/%s/commits/master?page=101' % software]
+            self.rules =[ 
+        Rule(LinkExtractor(allow=('https://github.com/torvalds/linux/commits/master\?page=\d+'),restrict_xpaths=('//div[@class="pagination"]/a[last()]'))),
+        Rule(LinkExtractor(allow=('/torvalds/linux/commit/[a-f0-9]+'),restrict_xpaths=('//a[@class="message"]')), callback='parse_item', follow=True),
+        ]
         super(GithubspiderSpider,self).__init__()
     def parse_item(self, response):
         item = CommitsspiderItem()
-        rawText = response.xpath('//p[@class="commit-title"]/text()|//div[@class="commit-desc"]/pre/text()|//div[@id="files"]').extract()
+        rawText = "".join(response.xpath('//p[@class="commit-title"]/text()|//div[@class="commit-desc"]/pre/text()|//div[@id="files"]').extract())
         item['cve'] = self.patt.findall(rawText)
-        if len(item) != 0:
+        if len(item['cve']) != 0:
             item['url'] = response.url
             yield item
         return
