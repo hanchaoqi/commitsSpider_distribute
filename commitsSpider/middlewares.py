9809ee07181428744b315dfe4c90b03a302f53eb
hanchaoqi <hanchao_qi@qq.com>
2016-04-30 05:43:59 -0400

    new random User Agent

---
+++ commitsSpider/middlewares.py
@@ -0,0 +1,11 @@
+import random
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+class RandomUserAgent(UserAgentMiddleware):
+    def __init__(self,agents):
+        self.agents = agents
+    @classmethod
+    def from_crawler(cls,crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+    def process_request(self,request,spider):
+        request.headers.setdefault('User-Agent',random.choice(self.agents))
