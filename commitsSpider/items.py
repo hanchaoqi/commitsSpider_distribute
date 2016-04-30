2aad8c0869d7e573a659d1be0ff2cc0f6e20471f
hanchaoqi <hanchao_qi@qq.com>
2016-04-30 12:18:49 -0400

    add ItemLoader

--- commitsSpider/items.py
+++ commitsSpider/items.py
@@ -6,10 +6,15 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose,TakeFirst
 
 class CommitsspiderItem(scrapy.Item):
     # define the fields for your item here like:
     cve = scrapy.Field()
     url = scrapy.Field()
     
+class CommitsspiderLoader(ItemLoader):
+    default_item_class = CommitsspiderItem
+    default_input_processor = MapCompose(lambda s:s.strip())
+    default_output_processor = TakeFirst()
